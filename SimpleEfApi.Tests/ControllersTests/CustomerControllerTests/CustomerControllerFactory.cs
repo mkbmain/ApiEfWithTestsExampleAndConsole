7523ccf29cd99868c5a1ba2f3c4e9f6cb1137cef
MkbMain <michael.bourke@live.co.uk>
2022-09-03 18:21:09 +0100

    Full tests and improvments

---
+++ SimpleEfApi.Tests/ControllersTests/CustomerControllerTests/CustomerControllerFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using ServiceLayer.Service.Customer;
+using SimpleEfApi.Controllers;
+
+namespace SimpleEfApi.Tests.ControllersTests.CustomerControllerTests
+{
+    internal static class CustomerControllerFactory
+    {
+        public static (CustomerController Sut, Mock<ICustomerService> mockCustomerService) Build()
+        {
+            var mockCustomerService = new Mock<ICustomerService>();
+            return (new CustomerController(mockCustomerService.Object), mockCustomerService);
+        }
+    }
+}
