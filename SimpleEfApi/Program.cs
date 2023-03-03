3ed3b1bb915802b41aedaeadeb70a8251763d5fc
mkbmain <michael.bourke@live.co.uk>
2023-03-03 14:59:43 +0000

    swagger dock

--- SimpleEfApi/Program.cs
+++ SimpleEfApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using ServiceLayer.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "SimpleEfApi", Version = "v1"
+    });
+});
+
 
 ServiceLayerIoC.Add(builder.Services, builder.Configuration);
 var app = builder.Build();
